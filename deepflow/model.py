b87cf74dbf54de791d7977c6b3a1dc72d1bf95d7
Xinyue Zhang <xinyue.zhang@tum.de>
2023-03-07 17:23:40 +0100

    add deepflow

---
+++ deepflow/model.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+
+
+class SimpleConvFactory(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        activation=nn.ReLU(),
+    ):
+        super(SimpleConvFactory, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size, stride=stride, padding=padding
+        )
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.activation = activation
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activation(x)
+        return x
+
+
+class DualDownsampleFactory(nn.Module):
+    def __init__(self, in_channels, ch_3x3):
+        super(DualDownsampleFactory, self).__init__()
+        # conv 3x3
+        self.conv = SimpleConvFactory(
+            in_channels, ch_3x3, kernel_size=3, stride=2, padding=1
+        )
+        # pool
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        conv = self.conv(x)
+        pool = self.pool(x)
+        # concat
+        x = torch.cat([conv, pool], dim=1)
+        return x
+
+
+class DualFactory(nn.Module):
+    def __init__(self, in_channels, ch_1x1, ch_3x3):
+        super(DualFactory, self).__init__()
+        # 1x1
+        self.conv1x1 = SimpleConvFactory(in_channels, ch_1x1, kernel_size=1)
+        # 3x3
+        self.conv3x3 = SimpleConvFactory(in_channels, ch_3x3, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        conv1x1 = self.conv1x1(x)
+        conv3x3 = self.conv3x3(x)
+        # concat
+        x = torch.cat([conv1x1, conv3x3], dim=1)
+        return x
+
+
+class DeepFlow(nn.Module):
+    def __init__(self):
+        super(DeepFlow, self).__init__()
+        self.conv1 = SimpleConvFactory(
+            in_channels=3, out_channels=96, kernel_size=3, padding=1
+        )
+        self.in3a = DualFactory(in_channels=96, ch_1x1=32, ch_3x3=32)
+        self.in3b = DualFactory(in_channels=64, ch_1x1=32, ch_3x3=48)
+        self.in3c = DualDownsampleFactory(in_channels=80, ch_3x3=80)
+        self.in4a = DualFactory(in_channels=160, ch_1x1=112, ch_3x3=48)
+        self.in4b = DualFactory(in_channels=160, ch_1x1=96, ch_3x3=64)
+        self.in4c = DualFactory(in_channels=160, ch_1x1=80, ch_3x3=80)
+        self.in4d = DualFactory(in_channels=160, ch_1x1=48, ch_3x3=96)
+        self.in4e = DualDownsampleFactory(in_channels=144, ch_3x3=96)
+        self.in5a = DualFactory(in_channels=240, ch_1x1=176, ch_3x3=160)
+        self.in5b = DualFactory(in_channels=336, ch_1x1=176, ch_3x3=160)
+        self.in6a = DualDownsampleFactory(in_channels=336, ch_3x3=96)
+        self.in6b = DualFactory(in_channels=432, ch_1x1=176, ch_3x3=160)
+        self.in6c = DualFactory(in_channels=336, ch_1x1=176, ch_3x3=160)
+        self.pool = nn.AvgPool2d(kernel_size=8)
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(in_features=5376, out_features=5)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.in3a(x)
+        x = self.in3b(x)
+        x = self.in3c(x)
+        x = self.in4a(x)
+        x = self.in4b(x)
+        x = self.in4c(x)
+        x = self.in4d(x)
+        x = self.in4e(x)
+        x = self.in5a(x)
+        x = self.in5b(x)
+        x = self.in6a(x)
+        x = self.in6b(x)
+        x = self.in6c(x)
+        x = self.pool(x)
+        x = self.flatten(x)
+        
+        x = self.fc(x)
+        x = self.softmax(x)
+        return x
---
+++ deepflow/train.py
@@ -0,0 +1,163 @@
+import torch
+import torchmetrics
+import os
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+from model import DeepFlow
+from dataset import Retinol
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+# define mode
+mode = "train"
+
+# define saved model path
+saved_path = None
+
+# define dataset path
+annotations_file = (
+    "/home/icb/xinyue.zhang/diabetic-retinopathy-detection/trainLabels.csv.zip"
+)
+img_dir = "/home/icb/xinyue.zhang/diabetic-retinopathy-detection/train"
+
+# device specification
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# define hyperparameters
+train_ratio = 0.8
+num_epoch = 1
+batch_size = 64
+learning_rate = 0.01
+momentum = 0.9
+weight_decay = 0.0005
+factor_epoch = 85
+lr_factor = 0.1
+
+# model output path
+model_prefix = "model/dual"
+
+
+transform = transforms.Compose(
+    [
+        transforms.ToPILImage(),
+        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop((2048, 2048)),
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+    ]
+)
+
+
+# Get dataset and dataloader
+full_dataset = Retinol(annotations_file, img_dir=img_dir, transform=transform)
+train_size = int(train_ratio * len(full_dataset))
+test_size = len(full_dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(
+    full_dataset, [train_size, test_size], generator=torch.Generator().manual_seed(42)
+)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+
+def train_ifc():
+
+    model = DeepFlow()
+    model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=learning_rate,
+        momentum=momentum,
+        weight_decay=weight_decay,
+    )
+    min_valid_loss = np.inf
+    for epoch in range(num_epoch):
+
+        train_loss = 0.0
+        for i, data in enumerate(tqdm(train_dataloader, 0)):
+            start = torch.cuda.Event(enable_timing=True)
+            end = torch.cuda.Event(enable_timing=True)
+
+            start.record()
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            acc = torchmetrics.Accuracy(task="multiclass", num_classes=5).to(device)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+
+            end.record()
+            torch.cuda.synchronize()
+            print(f"one iter: {start.elapsed_time(end)/60000}")  # minutes
+
+            if i % 50 == 49:  # print every 1000 mini-batches
+                print(
+                    f"[Epoch {epoch + 1}, iter {i + 1:5d}] acc: {acc(outputs, labels)}, loss: {train_loss / 50:.3f}"
+                )
+                train_loss = 0.0
+
+                valid_loss = 0.0
+                model.eval()  # Optional when not using Model Specific layer
+                for inputs, labels in test_dataloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    # Forward Pass
+                    outputs = model(inputs)
+                    # Find the Loss
+                    loss = criterion(outputs, labels)
+                    # Calculate Loss
+                    valid_loss += loss.item()
+                if min_valid_loss > valid_loss:
+                    PATH = os.path.join(model_prefix, f"epoch_{epoch}_iter_{i}.pth")
+                    print(
+                        f"Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f})"
+                    )
+                    print(f"Saving model to: {PATH}")
+                    torch.save(model.state_dict(), PATH)
+                    min_valid_loss = valid_loss
+
+    print("Finished Training")
+
+
+def predict(saved_path):
+    model = DeepFlow()
+    model.load_state_dict(torch.load(saved_path))
+    model.eval()
+
+    features = {}
+
+    def get_features(name):
+        def hook(model, input, output):
+            features[name] = output.detach()
+
+        return hook
+
+    model.fc2.register_forward_hook(get_features("flatten"))
+
+    for inputs, labels in test_dataloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # Forward Pass
+        outputs = model(inputs)
+
+        print(outputs.shape)
+
+        # np.savetxt("features.csv", out[1], fmt='%.5f', delimiter=",")
+        # print("Prediction completed, outputs dumped!")
+
+
+if __name__ == "__main__":
+    if mode == "train":
+        print(f"Using {device} to train the model")
+        train_ifc()
+    else:
+        print(f"Using {device} to get the features")
+        predict()
