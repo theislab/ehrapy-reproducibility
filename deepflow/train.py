f69194e4fbe7b28439090d679e67388b21d46258
Xinyue Zhang <xinyue.zhang@tum.de>
2023-03-08 12:13:11 +0100

    update

--- deepflow/train.py
+++ deepflow/train.py
@@ -5,16 +5,24 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 import torch.nn as nn
 import numpy as np
+import pandas as pd
 from model import DeepFlow
 from dataset import Retinol
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--mode", type=str, default="train", help="train or predict", required=True
+)
+args = parser.parse_args()
 
 # define mode
-mode = "train"
+mode = args.mode
 
 # define saved model path
-saved_path = None
+saved_path = "/home/icb/xinyue.zhang/ehrapy-reproducibility/deepflow/model/dual/epoch_0_iter_10.pth"
 
 # define dataset path
 annotations_file = (
@@ -26,18 +34,19 @@ img_dir = "/home/icb/xinyue.zhang/diabetic-retinopathy-detection/train"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # define hyperparameters
-train_ratio = 0.8
-num_epoch = 1
-batch_size = 64
+train_ratio = 0.98
+num_epoch = 50
+batch_size = 32
 learning_rate = 0.01
 momentum = 0.9
 weight_decay = 0.0005
 factor_epoch = 85
 lr_factor = 0.1
+patience = 5
 
 # model output path
 model_prefix = "model/dual"
-
+os.makedirs(model_prefix, exist_ok=True)
 
 transform = transforms.Compose(
     [
@@ -72,67 +81,57 @@ def train_ifc():
         momentum=momentum,
         weight_decay=weight_decay,
     )
+    acc = torchmetrics.Accuracy(task="multiclass", num_classes=5).to(device)
     min_valid_loss = np.inf
+    early_stop = 0
     for epoch in range(num_epoch):
-
         train_loss = 0.0
         for i, data in enumerate(tqdm(train_dataloader, 0)):
-            start = torch.cuda.Event(enable_timing=True)
-            end = torch.cuda.Event(enable_timing=True)
-
-            start.record()
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
-            # zero the parameter gradients
-            optimizer.zero_grad()
 
-            # forward + backward + optimize
+            optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-
-            acc = torchmetrics.Accuracy(task="multiclass", num_classes=5).to(device)
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
 
-            end.record()
-            torch.cuda.synchronize()
-            print(f"one iter: {start.elapsed_time(end)/60000}")  # minutes
-
-            if i % 2 == 1:  # print every 1000 mini-batches
+            # print training and validation loss for every 50 iterations
+            if i % 50 == 49:
                 print(
-                    f"[Epoch {epoch + 1}, iter {i + 1:5d}] acc: {acc(outputs, labels)}, loss: {train_loss / 50:.3f}"
+                    f"[Epoch {epoch + 1}, iter {i + 1:5d}] acc: {acc(outputs, labels)}, Training loss: {train_loss / 50:.3f}"
                 )
-                
                 train_loss = 0.0
                 valid_loss = 0.0
-                
-                model.eval()  # Optional when not using Model Specific layer
-                for i, data in enumerate(test_dataloader):
+                model.eval()
+                for _, data in enumerate(test_dataloader):
                     inputs, labels = data
                     inputs, labels = inputs.to(device), labels.to(device)
-                
-                    # Forward Pass
                     outputs = model(inputs)
-                    # Find the Loss
                     loss = criterion(outputs, labels)
-                    # Calculate Loss
                     valid_loss += loss.item()
-                    if i == 0:
-                        valid_outputs = outputs
-                        valid_labels =labels
-                    valid_outputs.append(outputs)
-                    valid_labels.append(labels)
-                valid_acc = acc(valid_outputs, valid_labels)
-                print(f"validation accuracy: {valid_acc}")
+                print(
+                    f"[Epoch {epoch + 1}, iter {i + 1:5d}] Validation loss: {valid_loss / 50:.3f}"
+                )
                 if min_valid_loss > valid_loss:
-                    PATH = os.path.join(model_prefix, f"epoch_{epoch}_iter_{i}.pth")
+                    early_stop = 0
                     print(
                         f"Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f})"
                     )
-                    print(f"Saving model to: {PATH}")
-                    torch.save(model.state_dict(), PATH)
                     min_valid_loss = valid_loss
+                else:
+                    early_stop += 1
+                    if early_stop == patience:
+                        PATH = os.path.join(model_prefix, f"epoch_{epoch}_iter_{i}.pth")
+                        print(f"Early Stop! Saving model to: {PATH}")
+                        torch.save(model.state_dict(), PATH)
+
+
+
+        PATH = os.path.join(model_prefix, f"epoch_{epoch}.pth")
+        print(f"Saving model to: {PATH}")
+        torch.save(model.state_dict(), PATH)
 
     print("Finished Training")
 
@@ -140,18 +139,31 @@ def train_ifc():
 def predict(saved_path):
     model = DeepFlow()
     model.load_state_dict(torch.load(saved_path))
-    model.eval()
+    model.to(device)
 
-    for inputs, labels in test_dataloader:
-        inputs, labels = inputs.to(device), labels.to(device)
+    model.eval()
+    for i, data in enumerate(tqdm(test_dataloader, 0)):
+        _, labels = data
+        inputs, labels = data
+        inputs = inputs.to(device)
 
         # Forward Pass
         outputs = model.first_part(inputs)
+        outputs = outputs.to("cpu")
+        outputs = pd.DataFrame(outputs.detach().numpy())
+        labels = pd.DataFrame(labels.detach().numpy())
+        print(labels)
 
-        print(outputs.shape)
-        break
-        # np.savetxt("features.csv", out[1], fmt='%.5f', delimiter=",")
-        # print("Prediction completed, outputs dumped!")
+        if i == 0:
+            test_outputs = outputs
+            test_labels = labels
+        else:
+            test_outputs = pd.concat((test_outputs, outputs), axis=0)
+            test_labels = pd.concat((test_labels, labels), axis=0)
+
+    test_labels.to_csv("labels.csv", sep=",")
+    test_outputs.to_csv("features.csv", sep=",")
+    print("Prediction completed, outputs dumped!")
 
 
 if __name__ == "__main__":
@@ -160,4 +172,4 @@ if __name__ == "__main__":
         train_ifc()
     else:
         print(f"Using {device} to get the features")
-        predict()
+        predict(saved_path)
